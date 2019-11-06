7fdf990e5f310aaae26a9f39319539739c74a184
victor-denisenko <victor.dens@mail.ru>
2019-11-06 15:30:14 +0300

    update activity_main.xml to test fragments

--- sample/src/main/java/com/github/florent37/inlineactivityresult/sample/MainActivity.kt
+++ sample/src/main/java/com/github/florent37/inlineactivityresult/sample/MainActivity.kt
@@ -2,7 +2,10 @@ package com.github.florent37.inlineactivityresult.sample
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.github.florent37.inlineactivityresult.sample.fragment.*
 import com.github.florent37.runtimepermission.RuntimePermission
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,32 +16,67 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 class MainActivity : AppCompatActivity() {
 
+    private val fragmentTag: String = "TESTFRAGMENT"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rx.setOnClickListener {
+        if (supportFragmentManager.fragments.any { it.tag.equals(fragmentTag) && it.isAdded }) {
+            menu.visibility = View.GONE
+        }
+
+        rxActivity.setOnClickListener {
             open(MainActivityRx::class.java)
         }
 
-        java7.setOnClickListener {
+        rxFragment.setOnClickListener {
+            openFragment(MainFragmentRx())
+        }
+
+        java7Activity.setOnClickListener {
             open(MainActivityJava7::class.java)
         }
 
-        java8.setOnClickListener {
+        java7Fragment.setOnClickListener {
+            openFragment(MainFragmentJava7())
+        }
+
+        java8Activity.setOnClickListener {
             open(MainActivityJava8::class.java)
         }
 
-        kotlin.setOnClickListener {
+        java8Fragment.setOnClickListener {
+            openFragment(MainFragmentJava8())
+        }
+
+        kotlinActivity.setOnClickListener {
             open(MainActivityKotlin::class.java)
         }
 
-        kotlinCoroutine.setOnClickListener {
+        kotlinFragment.setOnClickListener {
+            openFragment(MainFragmentKotlin())
+        }
+
+        kotlinCoroutineActivity.setOnClickListener {
             open(MainActivityKotlinCoroutine::class.java)
         }
 
-        kotlinCoroutine.setOnClickListener {
-            open(MainActivityKotlinCoroutine::class.java)
+        kotlinCoroutineFragment.setOnClickListener {
+            openFragment(MainFragmentKotlinCoroutine())
+        }
+
+    }
+
+    override fun onBackPressed() {
+        val visibleFragments = supportFragmentManager.fragments.filter { it.tag.equals(fragmentTag) && it.isVisible }
+
+        if (visibleFragments.isEmpty()) {
+            super.onBackPressed()
+        } else {
+            visibleFragments.forEach { removeFragment(it) }
+
+            menu.visibility = View.VISIBLE
         }
     }
 
@@ -49,4 +87,31 @@ class MainActivity : AppCompatActivity() {
                 }
                 .ask()
     }
+
+    private fun openFragment(fragment: Fragment) {
+        RuntimePermission.askPermission(this)
+                .onAccepted {
+                    setFragment(fragment)
+                }
+                .ask()
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        with(supportFragmentManager.beginTransaction()) {
+            replace(android.R.id.content, fragment, fragmentTag)
+
+            commit()
+        }
+
+        menu.visibility = View.GONE
+    }
+
+    private fun removeFragment(fragment: Fragment) {
+        with(supportFragmentManager.beginTransaction()) {
+            remove(fragment)
+
+            commit()
+        }
+    }
+
 }
