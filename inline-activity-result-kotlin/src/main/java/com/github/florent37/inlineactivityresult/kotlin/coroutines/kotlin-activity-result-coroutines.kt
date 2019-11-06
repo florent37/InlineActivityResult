cd57c72b6a850bda7bfabf3a467a011e94fa0061
victor-denisenko <victor.dens@mail.ru>
2019-11-06 15:20:19 +0300

    create startForResult methods with Request argument in kotlin-activity-result-coroutines

--- inline-activity-result-kotlin/src/main/java/com/github/florent37/inlineactivityresult/kotlin/coroutines/kotlin-activity-result-coroutines.kt
+++ inline-activity-result-kotlin/src/main/java/com/github/florent37/inlineactivityresult/kotlin/coroutines/kotlin-activity-result-coroutines.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.github.florent37.inlineactivityresult.InlineActivityResult
+import com.github.florent37.inlineactivityresult.request.Request
 import com.github.florent37.inlineactivityresult.Result
 import com.github.florent37.inlineactivityresult.kotlin.InlineActivityException
 import com.github.florent37.inlineactivityresult.kotlin.InlineActivityResultException
@@ -47,6 +48,45 @@ suspend fun Fragment.startForResult(intent: Intent): Result = suspendCoroutine {
                     }
                 }
                 .startForResult(intent)
+    }
+}
+
+suspend fun FragmentActivity.startForResult(request: Request): Result = suspendCoroutine { continuation ->
+    var resumed = false
+    InlineActivityResult(this)
+            .onSuccess { result ->
+                if (!resumed) {
+                    resumed = true
+                    continuation.resume(result)
+                }
+            }
+            .onFail { result ->
+                if (!resumed) {
+                    resumed = true
+                    continuation.resumeWithException(InlineActivityResultException(result))
+                }
+            }
+            .startForResult(request)
+}
+
+suspend fun Fragment.startForResult(request: Request): Result = suspendCoroutine { continuation ->
+    var resumed = false
+    when (activity) {
+        null -> continuation.resumeWithException(InlineActivityException())
+        else -> InlineActivityResult(activity)
+                .onSuccess { result ->
+                    if (!resumed) {
+                        resumed = true
+                        continuation.resume(result)
+                    }
+                }
+                .onFail { result ->
+                    if (!resumed) {
+                        resumed = true
+                        continuation.resumeWithException(InlineActivityResultException(result))
+                    }
+                }
+                .startForResult(request)
     }
 }
 
